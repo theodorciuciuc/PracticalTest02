894decfdfa0aff982efafbb79d19f77d7ca640a7
theodorciuciuc <ciuciuc.teodor@gmail.com>
2022-05-30 12:24:22 +0300

    task2

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+public interface Constants {
+
+    final public static String EUR_URL = "https://api.coindesk.com/v1/bpi/currentprice/EUR.json";
+    final public static String USD_URL = "https://api.coindesk.com/v1/bpi/currentprice/USD.json";
+
+}
